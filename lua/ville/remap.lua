1cb324aee5dfc6c3f2a42a62a9f179404ae1b90b
Vilhelm <v.eriksson@allgon.com>
2024-05-15 16:50:17 +0200

    added keymappings for resizing splits

--- lua/ville/remap.lua
+++ lua/ville/remap.lua
@@ -33,6 +33,12 @@ vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
+
+--Resize splits
+vim.keymap.set("n", "=", [[<cmd>vertical resize +5<cr>]]) -- make the window biger vertically
+vim.keymap.set("n", "-", [[<cmd>vertical resize -5<cr>]]) -- make the window smaller vertically
+vim.keymap.set("n", "+", [[<cmd>horizontal resize +2<cr>]]) -- make the window bigger horizontally by pressing shift and =
+vim.keymap.set("n", "_", [[<cmd>horizontal resize -2<cr>]]) -- make the window smaller horizontally by pressing shift and -
 --Exit terminal mode with escape
 vim.api.nvim_set_keymap('t','<ESC>','<C-\\><C-n>',{noremap = true})
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
